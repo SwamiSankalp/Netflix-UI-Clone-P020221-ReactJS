6ffee5ac3ac49ae4276ac9225b0da96fcd990c44
SankalpSwami <sankalpswami0102@gmail.com>
2021-02-28 19:23:05 +0530

    Firebase Deployment

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
@@ -7,22 +8,20 @@ import Nav from "./Nav";
 function App() {
   return (
     <div className="app">
-      {/* Navbar */}
       <Nav />
-      {/* Banner */}
       <Banner />
       <Row
-        title="Netflix Originals"
-        fetchUrl={requests.fetchNetflixOriginals}
+        title="NETFLIX ORIGINALS"
         isLargeRow
+        fetchUrl={requests.fetchNetflixOriginals}
       />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Top Rated Movies" fetchUrl={requests.fetchTopRated} />
+      <Row title="Trending" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Documentary Movies" fetchUrl={requests.fetchDocumentaries} />
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+/** base url to make request to the themoviedatabase */
+
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
 });
